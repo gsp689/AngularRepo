d3cfc166baa33310532207f3d5dafa15989bd849
ganeshsp <ganesh.patil@objectedge.com>
2017-11-02 11:27:29 +0530

    form validation and controller filters

--- AngularTutorials/WebContent/js/custom.js
+++ AngularTutorials/WebContent/js/custom.js
@@ -1,10 +1,52 @@
-var app = angular.module("firstModule",[]);
-app.controller("firstController", function($scope){
- $scope.message = "Hi, Guys!!!!";	
+var app = angular.module("firstModule", []);
+app.controller("firstController", function($scope) {
+	$scope.message = "Hi, Guys!!!!";
 });
 
+app.controller("dataController", function($scope) {
+	$scope.listOfEmployees = [ {
+		no : 1,
+		name : "ganesh",
+		surName : "patil",
+		age : "10",
+		salary : 12
+	}, {
+		no : 1,
+		name : "chetan",
+		surName : "ralkar",
+		age : "10",
+		salary : 1243
+	}, {
+		no : 1,
+		name : "mayur",
+		surName : "shetty",
+		age : "15",
+		salary : 3412
+	}, {
+		no : 1,
+		name : "manoj",
+		surName : "navale",
+		age : "18",
+		salary : 1342
+	}, {
+		no : 1,
+		name : "saurabh",
+		surName : "dhumal",
+		age : "05",
+		salary : 13452
+	} ];
 
-app.controller("dataController", function($scope){
-	$scope.listOfEmployees = [{fName: "ganesh",sal:12},{fName:"sunil",sal:20},{fName:"Anil",sal:120}];
+});
+
+app.controller("formController",function($scope){
+	$scope.reset = function() {
+		$scope.no="";
+		$scope.name="";
+		$scope.surname="";
+		$scope.age="";
+		$scope.salary="";
+		
+	}
+	
 	
 });
